f26a6efdd4c5299c82cb075db024003b10078cef
Felipe Savoia <felipe.savoia@forescout.com>
2023-09-28 10:27:00 +0200

    fixing variable name

--- app/oauth2.py
+++ app/oauth2.py
@@ -13,11 +13,11 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def create_access_token(data: dict):
-    to_ecnode = data.copy()
+    to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_ecnode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_ecnode, SECRET_KEY, algorithm=ALGORITHM)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
 
